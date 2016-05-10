958b89d835e713fe0e99b0b514dbb92368bf6f89
BestKora <developer@bestkora.com>
2016-05-10 13:29:44 +0300

    Адаптивный UI

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -20,6 +20,33 @@ class ViewController: UIViewController {
     private var userIsInTheMiddleOfTyping = false
     let decimalSeparator = formatter.decimalSeparator ?? "."
     
+    @IBOutlet weak var stack0: UIStackView!
+    @IBOutlet weak var stack1: UIStackView!
+    @IBOutlet weak var stack2: UIStackView!
+    @IBOutlet weak var stack3: UIStackView!
+    @IBOutlet weak var stack4: UIStackView!
+    @IBOutlet weak var stack5: UIStackView!
+    @IBOutlet weak var stack6: UIStackView!
+    
+    @IBOutlet weak var sin_1: UIButton!
+    @IBOutlet weak var cos_1: UIButton!
+    @IBOutlet weak var tan_1: UIButton!
+    @IBOutlet weak var x_2: UIButton!
+    @IBOutlet weak var plusMinusButton: UIButton!
+    
+    private lazy var buttonBlank:UIButton = {
+        let button = UIButton(frame: CGRectMake(100, 400, 100, 50))
+        button.backgroundColor = UIColor.blackColor()
+        button.setTitle("", forState: UIControlState.Normal)
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView(traitCollection.verticalSizeClass, buttonBlank: buttonBlank)
+        
+    }
+    
     @IBAction private func touchDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTyping {
@@ -96,7 +123,32 @@ class ViewController: UIViewController {
             performOperation(sender)
         }
     }
-
-
+    override func willTransitionToTraitCollection(newCollection: UITraitCollection,
+                     withTransitionCoordinator coordinator:UIViewControllerTransitionCoordinator) {
+        
+        super.willTransitionToTraitCollection(newCollection,
+                                              withTransitionCoordinator: coordinator)
+        configureView(newCollection.verticalSizeClass,buttonBlank:buttonBlank)
+    }
+    
+    private func configureView(verticalSizeClass: UIUserInterfaceSizeClass, buttonBlank:UIButton) {
+        if (verticalSizeClass == .Compact)  {
+            stack2.addArrangedSubview(plusMinusButton)
+            stack3.addArrangedSubview(sin_1)
+            stack4.addArrangedSubview(cos_1)
+            stack5.addArrangedSubview(tan_1)
+            stack6.addArrangedSubview(x_2)
+            stack0.addArrangedSubview(buttonBlank)
+            stack1.hidden = true
+        } else {
+            stack1.hidden = false
+            stack1.addArrangedSubview(plusMinusButton)
+            stack1.addArrangedSubview(sin_1)
+            stack1.addArrangedSubview(cos_1)
+            stack1.addArrangedSubview(tan_1)
+            stack1.addArrangedSubview(x_2)
+            stack0.removeArrangedSubview(buttonBlank)
+        }
+    }
 }
 
